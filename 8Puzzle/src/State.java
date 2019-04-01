cdadb1235eebb6259245f6e9e2e6a37a14734381
zbednar-18 <zbednar18@gmail.com>
2019-04-01 14:22:49 -0400

    Tom's Changes - Node and State classes completed.

---
+++ 8Puzzle/src/State.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+/**
+ * 
+ * @author Thomas Zanecosky
+ * @author Zackery Bednar
+ * 
+ * The state interface. Gets a method to check if goal is found, then creates
+ * a successor list, and finds the cost of the current state.
+ *
+ */
+public interface State {
+	boolean isGoalMet();
+	
+	ArrayList<State> createNext();
+	
+	double getCost();
+	
+	void print();
+	
+	boolean equal(State s);
+	
+	
+}
